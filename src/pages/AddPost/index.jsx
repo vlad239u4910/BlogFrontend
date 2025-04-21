7f17e2dd6fc9eab30d6e41e7189cec1305724910
vald1113 <vladik2006jr@gmail.com>
2025-04-21 11:57:27 +0200

    initial commit

---
+++ src/pages/AddPost/index.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+import { useNavigate, Navigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import SimpleMDE from "react-simplemde-editor";
+
+import "easymde/dist/easymde.min.css";
+import { selectIsAuth } from "../../redux/slices/auth";
+import axios from "../../axios";
+import styles from "./AddPost.module.scss";
+
+export const AddPost = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const isAuth = useSelector(selectIsAuth);
+  const [isLoading, setLoading] = React.useState(false);
+  const [text, setText] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [tags, setTags] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+  const inputFileRef = React.useRef(null);
+
+  const isEditing = Boolean(id);
+
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append("image", file);
+      const { data } = await axios.post("/upload", formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert("Error to loading file!");
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl("");
+  };
+
+  const onChange = React.useCallback((value) => {
+    setText(value);
+  }, []);
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
+
+      const fields = {
+        title,
+        imageUrl,
+        tags,
+        text,
+      };
+
+      const { data } = isEditing
+        ? await axios.patch(`/posts/${id}`, fields)
+        : await axios.post("/posts", fields);
+
+      const _id = isEditing ? id : data._id;
+
+      navigate(`/posts/${_id}`);
+    } catch (err) {
+      console.warn(err);
+      alert("Error creating post!");
+    }
+  };
+
+  React.useEffect(() => {
+    if (id) {
+      axios
+        .get(`/posts/${id}`)
+        .then(({ data }) => {
+          setTitle(data.title);
+          setText(data.text);
+          setImageUrl(data.imageUrl);
+          setTags(data.tags.join(","));
+        })
+        .catch((err) => {
+          console.warn(err);
+          alert("Error getting post!");
+        });
+    }
+  }, []);
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: "400px",
+      autofocus: true,
+      placeholder: "Введите текст...",
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    []
+  );
+
+  if (!window.localStorage.getItem("token") && !isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
+        Upload image
+      </Button>
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
+      {imageUrl && (
+        <>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Delete
+          </Button>
+          <img
+            className={styles.image}
+            src={`${process.env.REACT_APP_API_URL}${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
+      )}
+      <br />
+      <br />
+      <TextField
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        classes={{ root: styles.title }}
+        variant="standard"
+        placeholder="Post title..."
+        // value={fields.title}
+        // onChange={(e) => setFieldValue("title", e.target.value)}
+        fullWidth
+      />
+      <TextField
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        classes={{ root: styles.tags }}
+        variant="standard"
+        placeholder="Tags"
+        fullWidth
+      />
+      <SimpleMDE
+        className={styles.editor}
+        value={text}
+        onChange={onChange}
+        options={options}
+      />
+      <div className={styles.buttons}>
+        <Button onClick={onSubmit} size="large" variant="contained">
+          {isEditing ? "Save" : "Post"}
+        </Button>
+        <a href="/">
+          <Button size="large">Cancel</Button>
+        </a>
+      </div>
+    </Paper>
+  );
+};
