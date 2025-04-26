cf3422fb12e90e14c4694bc7d5ba727087586457
vald1113 <vladik2006jr@gmail.com>
2025-04-26 18:11:15 +0200

    tab value

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ export const Home = () => {
     <>
       <Tabs
         style={{ marginBottom: 15 }}
-        value={0}
+        value={tabValue}
         onChange={handleChangeTab}
         aria-label="basic tabs example"
       >
